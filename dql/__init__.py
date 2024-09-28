97c1595b5cf6e83a18b73a519377e3fdbed58bd5
Sai Jonnala <jspreddy@users.noreply.github.com>
2024-09-28 14:24:33 -0400

    Bump version: 0.6.4-dev1 → 0.6.4-dev2

--- dql/__init__.py
+++ dql/__init__.py
@@ -7,7 +7,7 @@ import os
 from .cli import DQLClient
 from .engine import Engine, FragmentEngine
 
-__version__ = "0.6.4-dev1"
+__version__ = "0.6.4-dev2"
 __all__ = ["Engine", "FragmentEngine", "DQLClient"]
 
 LOG_CONFIG = {
