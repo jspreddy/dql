5cf556fd522fc6538e5fafd80f5dc3e2478967e7
Sai Jonnala <sai.jonnala@passportinc.com>
2024-10-17 18:21:15 -0400

    Bump version: 0.6.4-dev5 → 0.6.4-dev6

--- dql/__init__.py
+++ dql/__init__.py
@@ -7,7 +7,7 @@ import os
 from .cli import DQLClient
 from .engine import Engine, FragmentEngine
 
-__version__ = "0.6.4-dev5"
+__version__ = "0.6.4-dev6"
 __all__ = ["Engine", "FragmentEngine", "DQLClient"]
 
 LOG_CONFIG = {
