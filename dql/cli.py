6251b1890c9a3eee14b3de31fe444e1c17075213
Sai Jonnala <jspreddy@users.noreply.github.com>
2026-06-26 13:12:03 -0400

    Bump version: 0.6.4-dev11 → 0.6.4-dev12

--- dql/cli.py
+++ dql/cli.py
@@ -60,7 +60,7 @@ from .output import (
 )
 from .throttle import TableLimits
 
-__version__ = "0.6.4-dev11"
+__version__ = "0.6.4-dev12"
 
 # From http://docs.aws.amazon.com/general/latest/gr/rande.html#ddb_region
 REGIONS = [
